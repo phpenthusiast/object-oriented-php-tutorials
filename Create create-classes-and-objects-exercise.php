11b4d7c7d3a1038a814df10f46c248bf43dd4fc7
PHPenthusiast <support@phpenthusiast.com>
2015-01-09 04:00:25 +0200

    Create Create create-classes-and-objects-exercise.php
    The solution for the exercise in the tutorial about creating classes and objects that can be found at: http://phpenthusiast.com/object-oriented-php-tutorials/create-classes-and-objects

---
+++ Create create-classes-and-objects-exercise.php
@@ -0,0 +1,28 @@
+<?php
+// Exercise solutions for tutorial number 1
+class User {
+  public $firstName;
+  public $lastName;
+ 
+  public function hello()
+  {
+    return "hello";
+  }
+}
+ 
+ 
+$user1 = new User();
+$user1->firstName = 'John';
+$user1->lastName  = 'Doe';
+ 
+$hello = $user1->hello();
+ 
+ 
+$user2 = new User();
+$user2 ->firstName = 'Jane';
+$user2 ->lastName  = 'Doe';
+ 
+ 
+echo $hello . ",  " . $user1->firstName . " " . $user1->lastName;
+ 
+echo $hello . ",  " . $user2->firstName . " " . $user2->lastName;
