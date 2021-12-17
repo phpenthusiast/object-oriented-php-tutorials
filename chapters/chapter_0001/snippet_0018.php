bbc432c7ab06ecfb8be981c2888aae084658fce1
PHPenthusiast <support@phpenthusiast.com>
2021-12-17 20:43:58 +0200

    Create snippet_0018.php

---
+++ chapters/chapter_0001/snippet_0018.php
@@ -0,0 +1,19 @@
+<?php
+class User 
+{
+    public $firstName;
+    public $lastName;
+
+    public function hello()
+    {
+        return "hello";
+    }
+}
+
+
+$user1 = new User();
+$user1 -> firstName = 'John';
+$user1 -> lastName  = 'Doe';
+
+echo $user1->firstName . " " . $user1->lastName;
+
