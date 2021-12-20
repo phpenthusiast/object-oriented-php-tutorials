e4ac1ee3f5bdd49393f32644f11ab12c1c10e5d9
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 12:34:35 +0200

    Update snippet_0006.php

--- chapters/chapter_0004/snippet_0006.php
+++ chapters/chapter_0004/snippet_0006.php
@@ -1,12 +1,8 @@
 <?php
 class User 
 {
+    // We use the private access modifier in order to
+    //   prevent code from outside
+    //   the class from modifying the property's value
     private $firstName;
-    
-    // A public setter method allows us to set the 
-    //   $firsName property.
-    public function setFirstName($str)
-    {
-        $this -> firstName = $str;
-    }
 }
