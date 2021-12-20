7cdc1f4d2ac23f8471ca5cdd8f5506b342ed165d
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 13:36:21 +0200

    Update snippet_0004.php

--- chapters/chapter_0005/snippet_0004.php
+++ chapters/chapter_0005/snippet_0004.php
@@ -1,1 +1,22 @@
 <?php
+class Car 
+{
+    private $model;
+    
+    // The constructor
+    public function __construct ($model)
+    {
+        $this -> model = $model;
+    }
+    
+    public function getCarModel()
+    {
+        return ' The car model is: ' . $this -> model;
+    }
+}
+
+// We pass the value of the variable 
+//   once we create the object
+$car1 = new Car("Mercedes");
+
+echo $car1 -> getCarModel();
