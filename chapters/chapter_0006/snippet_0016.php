18d7bf46291b8ce0fd1b1ba86369621382bb95a1
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 14:31:35 +0200

    Update snippet_0016.php

--- chapters/chapter_0006/snippet_0016.php
+++ chapters/chapter_0006/snippet_0016.php
@@ -1,1 +1,33 @@
 <?php
+class User 
+{
+    private $username;
+
+    public function setUsername($name)
+    {
+        $this -> username = $name;
+    }
+
+    public function getUsername()
+    {
+        return $this -> username;
+    }
+}
+
+
+class Admin extends User{
+    public function expressYourRole()
+    {
+        return strtolower(__CLASS__);
+    }
+
+    public function sayHello()
+    {
+        return "Hello admin, " . $this -> getUsername();
+    }
+}
+
+
+$admin1 = new Admin();
+$admin1 -> setUsername("Balthazar");
+echo $admin1 -> sayHello();
