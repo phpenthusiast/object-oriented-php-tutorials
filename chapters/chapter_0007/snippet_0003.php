c51c921e142a127ca6cd4e8f6f5cab6ecde715cb
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 15:06:26 +0200

    Update snippet_0003.php

--- chapters/chapter_0007/snippet_0003.php
+++ chapters/chapter_0007/snippet_0003.php
@@ -1,1 +1,15 @@
 <?php
+abstract class Car 
+{
+    // Abstract classes can have properties
+    protected $tankVolume;
+    
+    // Abstract classes can have non abstract methods
+    public function setTankVolume($volume)
+    {
+        $this -> tankVolume = $volume;
+    }
+    
+    // Abstract method
+    abstract public function calcNumMilesOnFullTank();
+}
