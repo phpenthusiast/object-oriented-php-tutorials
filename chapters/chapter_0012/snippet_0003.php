0f014ea943f1f59aaadf9152a7b36dc0587b9c7b
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:53:18 +0200

    Update snippet_0003.php

--- chapters/chapter_0012/snippet_0003.php
+++ chapters/chapter_0012/snippet_0003.php
@@ -1,1 +1,11 @@
 <?php
+class Utilis 
+{
+    static public $numCars = 0;
+
+    static public function addToNumCars($int)
+    {
+        $int = (int)$int;
+        self::$numCars += $int;
+    }
+}
