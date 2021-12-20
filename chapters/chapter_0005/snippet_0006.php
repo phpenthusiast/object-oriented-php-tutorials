8c1331ebcba2a53dbb75bdd83d65910955abd056
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 13:37:08 +0200

    Update snippet_0006.php

--- chapters/chapter_0005/snippet_0006.php
+++ chapters/chapter_0005/snippet_0006.php
@@ -1,1 +1,25 @@
 <?php
+class Car 
+{
+    private $model = '';
+
+    // The constructor
+    public function __construct($model = null)
+    {
+        if($model)
+        {
+            $this -> model = $model;
+        }
+    }
+    
+    public function getCarModel()
+    {
+        return ' The car model is: ' . $this -> model;
+    }
+}
+
+// We create the new Car object 
+//   with the value of the model
+$car1 = new Car('Mercedes');
+
+echo $car1 -> getCarModel();
