a617e45a097aefeab01e7529cd716353218aa7e7
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 15:06:45 +0200

    Update snippet_0004.php

--- chapters/chapter_0007/snippet_0004.php
+++ chapters/chapter_0007/snippet_0004.php
@@ -1,1 +1,12 @@
 <?php
+class Honda extends Car 
+{
+    // Since we inherited abstract method, 
+    //   we need to define it in the child class,
+    //   by adding code to the method's body.
+    public function calcNumMilesOnFullTank()
+    {
+        $miles = $this -> tankVolume*30;
+        return $miles;
+    }
+}
