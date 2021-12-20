94d4327e0128c3550e717ce745fe2e06b65364a0
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 13:36:45 +0200

    Update snippet_0005.php

--- chapters/chapter_0005/snippet_0005.php
+++ chapters/chapter_0005/snippet_0005.php
@@ -1,1 +1,27 @@
 <?php
+class Car 
+{
+    // The $model property has a default value of "N/A"
+    private $model = "";
+    
+    // We don't have to assign a value to the $model property 
+    //   since it already has a default value
+    public function __construct($model = null)
+    {
+        if($model)
+        { 
+            $this -> model = $model;
+        }
+    }
+    
+    public function getCarModel()
+    {
+        return ' The car model is: ' . $this -> model;
+    }
+}
+
+// We create the new Car object 
+//   without passing a value to the model
+$car1 = new Car();
+
+echo $car1 -> getCarModel();
