30abc3eb28b4b64b0e84ea8764f16f86f84cbed3
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 15:08:10 +0200

    Update snippet_0008.php

--- chapters/chapter_0007/snippet_0008.php
+++ chapters/chapter_0007/snippet_0008.php
@@ -1,1 +1,17 @@
 <?php
+abstract class User 
+{
+    protected $username = '';
+
+    abstract public function stateYourRole();
+
+    public function setUsername($name)
+    {
+        $this -> username = $name;
+    }
+
+    public function getUsername()
+    {
+        return $this -> username;
+    }
+}
