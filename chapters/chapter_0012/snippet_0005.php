9f55bdc533400152445446d3b463772b85b18e4f
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:54:39 +0200

    Update snippet_0005.php

--- chapters/chapter_0012/snippet_0005.php
+++ chapters/chapter_0012/snippet_0005.php
@@ -1,1 +1,24 @@
 <?php
+class Utilis 
+{
+    // Hold the number of cars
+    static public $numberOfCars = 0;
+
+    // Add 1 to the number of cars each time the method is called
+    static public function add1ToCars()
+    {
+        self::$numberOfCars++;
+    }
+}
+
+
+echo Utilis::$numberOfCars;
+
+Utilis::add1ToCars();
+echo Utilis::$numberOfCars;
+
+Utilis::add1ToCars();
+echo Utilis::$numberOfCars;
+
+Utilis::add1ToCars();
+echo Utilis::$numberOfCars;
