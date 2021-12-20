aa8d34e3b0e6d303ced025f9b5c8388132101b24
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:43:00 +0200

    Update snippet_0008.php

--- chapters/chapter_0011/snippet_0008.php
+++ chapters/chapter_0011/snippet_0008.php
@@ -1,1 +1,7 @@
 <?php
+// Type hinting ensures that the function gets only objects
+//   that belong to the Car interface
+function calcTankPrice(Car $car, $pricePerGalon)
+{
+    echo $car -> calcTankVolume() * 0.0043290 * $pricePerGalon . "$";
+}
