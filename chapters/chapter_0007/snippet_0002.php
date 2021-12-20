ad8bb107c15e9d73ce15c7eea55e91ff536d2d0e
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 15:06:03 +0200

    Update snippet_0002.php

--- chapters/chapter_0007/snippet_0002.php
+++ chapters/chapter_0007/snippet_0002.php
@@ -1,1 +1,7 @@
 <?php
+// Abstract classes are declared with the abstract keyword, 
+//   and contain abstract methods
+abstract class Car 
+{
+    abstract public function calcNumMilesOnFullTank();
+}
