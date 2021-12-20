5b3ab93d07e0d48dbf4dbd3dc80e514c4a9ef46b
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:39:30 +0200

    Update snippet_0002.php

--- chapters/chapter_0011/snippet_0002.php
+++ chapters/chapter_0011/snippet_0002.php
@@ -1,1 +1,6 @@
 <?php
+// Type hinting ensures that the function gets only Bmw objects as arguments
+function calcTankPrice(Bmw $bmw, $pricePerGalon)
+{
+    return $bmw -> calcTankVolume() * 0.0043290 * $pricePerGalon . "$";
+}
