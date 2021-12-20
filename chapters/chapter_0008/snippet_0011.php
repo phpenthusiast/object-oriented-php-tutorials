7af7e68f78ffde179cee9c20fabcbebba0ab9593
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:05:23 +0200

    Update snippet_0011.php

--- chapters/chapter_0008/snippet_0011.php
+++ chapters/chapter_0008/snippet_0011.php
@@ -1,1 +1,27 @@
 <?php
+class AuthorEditor extends User implements Author,Editor  
+{
+    private $authorPrivilegesArray = array();
+
+    private $editorPrivilegesArray = array();
+
+    public function setAuthorPrivileges($array)
+    {
+        $this -> authorPrivilegesArray = $array;
+    }
+
+    public function getAuthorPrivileges()
+    {
+        return $this -> authorPrivilegesArray;
+    }
+
+    public function setEditorPrivileges($array)
+    {
+        $this -> editorPrivilegesArray = $array;
+    }
+
+    public function getEditorPrivileges()
+    {
+        return $this -> editorPrivilegesArray;
+    }
+}
