5863acd2e8a008d46a8282c083f5ba2a14570f26
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 13:38:39 +0200

    Update snippet_0009.php

--- chapters/chapter_0005/snippet_0009.php
+++ chapters/chapter_0005/snippet_0009.php
@@ -1,1 +1,12 @@
 <?php
+class User 
+{
+    private $firstName;
+    private $lastName;
+
+    // Constructor function to set a single value
+    public function __construct($firstName)
+    {
+        $this -> firstName = $firstName;
+    }
+}
