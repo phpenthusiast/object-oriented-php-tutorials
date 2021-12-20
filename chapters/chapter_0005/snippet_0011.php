1b7a5def21b8a7c38ecac8ef8d292be9e2259400
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 13:39:25 +0200

    Update snippet_0011.php

--- chapters/chapter_0005/snippet_0011.php
+++ chapters/chapter_0005/snippet_0011.php
@@ -1,1 +1,18 @@
 <?php
+class User 
+{
+    private $firstName;
+    private $lastName;
+
+    // Constructor function to set more than one value
+    public function __construct($firstName,$lastName)
+    {
+        $this -> firstName = $firstName;
+        $this -> lastName = $lastName;
+    }
+
+    public function getFullName()
+    {
+        return $this -> firstName . ' ' . $this -> lastName;
+    }
+}
