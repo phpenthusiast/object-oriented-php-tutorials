1cdce01136b646038c1ac03e4c2af32574d487b1
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:00:51 +0200

    Update snippet_0005.php

--- chapters/chapter_0008/snippet_0005.php
+++ chapters/chapter_0008/snippet_0005.php
@@ -1,1 +1,27 @@
 <?php
+// The class implements two interfaces
+class miniCar implements Car, Vehicle 
+{
+    private $model;
+    private $hasWheels;
+
+    public function setModel($name)
+    {
+        $this -> model = $name;
+    }
+
+    public function getModel()
+    {
+        return $this -> model;
+    }
+
+    public function setHasWheels($bool)
+    {
+        $this -> hasWheels = $bool;
+    }
+
+    public function getHasWheels()
+    {
+        return ($this -> hasWheels)? "has wheels" : "no wheels";
+    }
+}
