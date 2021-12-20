99354321505c9f8d75f8a00ebaa3873845f37f2d
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:06:52 +0200

    Update snippet_0014.php

--- chapters/chapter_0008/snippet_0014.php
+++ chapters/chapter_0008/snippet_0014.php
@@ -1,1 +1,5 @@
 <?php
+$user1 = new AuthorEditor();
+$user1 -> setUsername("Balthazar");
+$user1 -> setAuthorPrivileges(array("write text", "add punctuation"));
+$user1 -> setEditorPrivileges(array("edit text", "edit punctuation"));
