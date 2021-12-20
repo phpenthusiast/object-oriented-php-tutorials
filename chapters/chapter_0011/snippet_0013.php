e960f08d8259669cb2f27fb2e0d28dfd68332b16
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:46:21 +0200

    Update snippet_0013.php

--- chapters/chapter_0011/snippet_0013.php
+++ chapters/chapter_0011/snippet_0013.php
@@ -1,1 +1,10 @@
 <?php
+$user1 = new Commentator();
+$user1 -> setUsername("Jane");
+$user1 -> setGender("female");
+echo addMrOrMrsToUsername($user1);
+
+$user2 = new Commentator();
+$user2 -> setUsername("Bob");
+$user2 -> setGender("male");
+echo addMrOrMrsToUsername($user2);
