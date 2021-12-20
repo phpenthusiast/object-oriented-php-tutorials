7311277772e3d35537569fa78e0f34ac0ab40245
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:29:05 +0200

    Update snippet_0002.php

--- chapters/chapter_0010/snippet_0002.php
+++ chapters/chapter_0010/snippet_0002.php
@@ -1,1 +1,11 @@
 <?php
+// The function can only get array as an argument.
+function calcNumMilesOnFullTank(array $models)
+{
+    foreach($models as $item){
+        echo $carModel = $item[0];
+        echo " : ";
+        echo $numberOfMiles = $item[1] * $item[2];
+        echo "<br />";
+    }
+}
