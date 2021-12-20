4b9601a973095c46f9cbc0c531c0cbf39948ae07
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:44:00 +0200

    Update snippet_0010.php

--- chapters/chapter_0011/snippet_0010.php
+++ chapters/chapter_0011/snippet_0010.php
@@ -1,1 +1,9 @@
 <?php
+interface User 
+{
+    public function setUsername($username);
+    public function getUsername();
+
+    public function setGender($gender);
+    public function getGender();
+}
