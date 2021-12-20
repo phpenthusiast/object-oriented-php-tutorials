a7de1c16dd7333858119746bc42366a326505b83
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 14:30:12 +0200

    Update snippet_0013.php

--- chapters/chapter_0006/snippet_0013.php
+++ chapters/chapter_0006/snippet_0013.php
@@ -1,1 +1,13 @@
 <?php
+class Admin extends User
+{
+    public function expressYourRole()
+    {
+        return strtolower(__CLASS__);
+    }
+    
+    public function sayHello()
+    {
+        return "Hello admin, " . $this -> username;
+    }
+}
