6e213c1bdf8ca702b3594a1501a7b71cdb72cd28
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 14:31:12 +0200

    Update snippet_0015.php

--- chapters/chapter_0006/snippet_0015.php
+++ chapters/chapter_0006/snippet_0015.php
@@ -1,1 +1,32 @@
 <?php
+// We can declare the $username property as protected to allow the 
+//   access for properties and methods that belongs to the child classes
+class User 
+{
+    // Declare the $username as protected
+    protected $username;
+
+    public function setUsername($name)
+    {
+        $this -> username = $name;
+    }
+}
+
+
+class Admin extends User 
+{
+    public function expressYourRole()
+    {
+        return strtolower(__CLASS__);
+    }
+
+    public function sayHello()
+    {
+        return "Hello admin, " . $this -> username;
+    }
+}
+
+
+$admin1 = new Admin();
+$admin1 -> setUsername("Balthazar");
+echo $admin1 -> sayHello();
