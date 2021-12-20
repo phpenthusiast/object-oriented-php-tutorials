26a42a5648a27075db318d41f3a171aee66ec6e5
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 15:07:05 +0200

    Update snippet_0005.php

--- chapters/chapter_0007/snippet_0005.php
+++ chapters/chapter_0007/snippet_0005.php
@@ -1,1 +1,16 @@
 <?php
+class Toyota extends Car 
+{
+    // Since we inherited abstract method, 
+    //   we need to define it in the child class, 
+    //   by adding code to the method's body.
+    public function calcNumMilesOnFullTank()
+    {
+        return $miles = $this -> tankVolume*33;
+    }
+
+    public function getColor()
+    {
+        return "beige";
+    }
+}
