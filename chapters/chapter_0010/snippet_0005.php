4f3c42ca9cc902360c4073196a85dd32721d3a54
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:30:43 +0200

    Update snippet_0005.php

--- chapters/chapter_0010/snippet_0005.php
+++ chapters/chapter_0010/snippet_0005.php
@@ -1,1 +1,18 @@
 <?php
+class Car 
+{
+    protected $driver;
+
+    // The constructor can only get Driver objects as arguments.
+    public function __construct(Driver $driver)
+    {
+        $this -> driver = $driver;
+    }
+}
+
+
+class Driver {}
+
+
+$driver1 = new Driver();
+$car1    = new Car($driver1);
