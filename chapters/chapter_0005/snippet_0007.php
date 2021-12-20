d8de45f713cf26f5e4745928631064ec5415ffbf
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 13:37:36 +0200

    Update snippet_0007.php

--- chapters/chapter_0005/snippet_0007.php
+++ chapters/chapter_0005/snippet_0007.php
@@ -1,1 +1,21 @@
 <?php
+class Car 
+{
+    private $model = '';
+    
+    // The constructor
+    public function __construct($model = null)
+    {
+        if($model)
+        {
+            $this -> model = $model;
+        }
+    }
+    
+    public function getCarModel()
+    {
+        // We use the __CLASS__ magic constant 
+        //   in order to get the class name
+        return " The <b>" . __CLASS__ . "</b> model is: " . $this -> model;
+    }
+}
