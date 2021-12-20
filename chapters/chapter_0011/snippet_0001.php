b35285e8e4431c29163decd54eedb4943495f83d
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:38:56 +0200

    Update snippet_0001.php

--- chapters/chapter_0011/snippet_0001.php
+++ chapters/chapter_0011/snippet_0001.php
@@ -1,1 +1,21 @@
 <?php
+class Bmw 
+{
+    protected $model;
+    protected $rib;
+    protected $height;
+
+    // The properties are introduced to the class through the constructor
+    public function __construct($model, $rib, $height)
+    {
+        $this -> model = $model;
+        $this -> rib = $rib;
+        $this -> height = $height;
+    }
+
+    // Calculate the tank volume for rectangular tanks
+    public function calcTankVolume()
+    {
+        return $this -> rib * $this -> rib * $this -> height;
+    }
+}
