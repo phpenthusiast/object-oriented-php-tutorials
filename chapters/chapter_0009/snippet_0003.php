6ed7f1e83543735171f08811cabe04821602577d
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:16:48 +0200

    Update snippet_0003.php

--- chapters/chapter_0009/snippet_0003.php
+++ chapters/chapter_0009/snippet_0003.php
@@ -1,1 +1,18 @@
 <?php
+class Rectangle implements Shape 
+{
+    private $width;
+    private $height;
+
+    public function __construct($width, $height)
+    {
+        $this -> width = $width;
+        $this -> height = $height;
+    }
+
+    // calcArea calculates the area of rectangles
+    public function calcArea()
+    {
+        return $this -> width * $this -> height;
+    }
+}
