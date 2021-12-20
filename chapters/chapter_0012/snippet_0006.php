1eb589b0a46a92b919dfac1e1c0455c95cfd75bf
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:55:12 +0200

    Update snippet_0006.php

--- chapters/chapter_0012/snippet_0006.php
+++ chapters/chapter_0012/snippet_0006.php
@@ -1,1 +1,11 @@
 <?php
+class Utilis 
+{
+    // The method uses PHP's header function 
+    //   to redirect the user
+    static public function redirect($url)
+    {
+        header("Location: $url");
+        exit;
+    }
+}
