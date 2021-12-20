23c76657b198b5ead164aef8adbfb8494e0eac2c
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 20:57:59 +0200

    Update snippet_0001.php

--- chapters/chapter_0008/snippet_0001.php
+++ chapters/chapter_0008/snippet_0001.php
@@ -1,1 +1,12 @@
 <?php
+interface interfaceName 
+{
+    // Abstract methods
+}
+
+
+class Child implements interfaceName 
+{
+    // Defines the interface methods,
+    //   and may have its own methods.
+}
