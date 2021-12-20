225bddbfc3bfd411331ca5ec6804728e8cf37907
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:40:38 +0200

    Update snippet_0004.php

--- chapters/chapter_0011/snippet_0004.php
+++ chapters/chapter_0011/snippet_0004.php
@@ -1,1 +1,20 @@
 <?php
+class Mercedes 
+{
+    protected $model;
+    protected $radius;
+    protected $height;
+
+    public function __construct($model, $radius, $height)
+    {
+        $this -> model = $model;
+        $this -> radius = $radius;
+        $this -> height = $height;
+    }
+
+    // Calculate the volume of cylinders
+    public function calcTankVolume()
+    {
+        return $this -> radius * $this -> radius * pi() * $this -> height;
+    }
+}
