aa4134082a1acdedcad6df72ff0e417ad9207c81
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:07:32 +0200

    Update snippet_0015.php

--- chapters/chapter_0008/snippet_0015.php
+++ chapters/chapter_0008/snippet_0015.php
@@ -1,1 +1,10 @@
 <?php
+$userName  = $user1 -> getUsername();
+$userPrivileges = array_merge($user1 -> getAuthorPrivileges(),
+    $user1 -> getEditorPrivileges());
+
+echo $userName . " has the following privileges: ";
+foreach($userPrivileges as $privilege) {
+    echo " {$privilege},";
+}
+echo ".";
