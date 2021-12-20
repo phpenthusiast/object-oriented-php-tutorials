5b5417e2a1d214e4773cfb286607ad9cfc54732b
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:45:57 +0200

    Update snippet_0012.php

--- chapters/chapter_0011/snippet_0012.php
+++ chapters/chapter_0011/snippet_0012.php
@@ -1,1 +1,17 @@
 <?php
+function addMrOrMrsToUsername(User $user)
+{
+    $userName   = $user -> getUsername();
+    $userGender = $user -> getGender();
+
+    if($userGender === 'female')
+    {
+        return "Mrs. " . $userName;
+    }
+    else if($userGender === 'male')
+    {
+        return "Mr. " . $userName;
+    }
+
+    return $userName;
+}
