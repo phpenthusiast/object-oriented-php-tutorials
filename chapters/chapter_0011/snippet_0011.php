89e9a1dacc0a8429da1dfc34548c158c560f0d52
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:45:12 +0200

    Update snippet_0011.php

--- chapters/chapter_0011/snippet_0011.php
+++ chapters/chapter_0011/snippet_0011.php
@@ -1,1 +1,33 @@
 <?php
+class Commentator implements User 
+{
+    private $username = '';
+    private $gender  = 'none of your business!';
+
+    // Set and get the $username
+    public function setUsername($name)
+    {
+        $this -> username = (is_string($name))? $name : 'N/A';
+    }
+
+    public function getUsername()
+    {
+        return $this -> username;
+    }
+
+    // Set and get the gender
+    public function setGender($gender)
+    {
+        $gendersArray = array('female', 'male', 'other');
+
+        if(in_array($gender, $gendersArray))
+        {
+            $this -> gender = $gender;
+        }
+    }
+
+    public function getGender()
+    {
+        return $this -> gender;
+    }
+}
