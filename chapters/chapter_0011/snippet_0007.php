fa38b31135ef78b3928957e33c3e7e2631b9f769
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:42:26 +0200

    Update snippet_0007.php

--- chapters/chapter_0011/snippet_0007.php
+++ chapters/chapter_0011/snippet_0007.php
@@ -1,1 +1,37 @@
 <?php
+class Bmw extends Car 
+{
+    protected $rib;
+
+    public function __construct($model, $rib, $height)
+    {
+        $this -> model = $model;
+        $this -> rib        = $rib;
+        $this -> height = $height;
+    }
+
+    // Calculate a rectangular tank volume
+    public function calcTankVolume()
+    {
+        return $this -> rib * $this -> rib * $this -> height;
+    }
+}
+
+
+class Mercedes extends Car {
+
+    protected $radius;
+
+    public function __construct($model, $radius, $height)
+    {
+        $this ->model = $model;
+        $this -> radius = $radius;
+        $this -> height = $height;
+    }
+
+    // Calculates the volume of cylinder
+    public function calcTankVolume()
+    {
+        return $this -> radius * $this -> radius * pi() * $this -> height;
+    }
+}
