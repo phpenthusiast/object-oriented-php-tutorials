27f468f6417e2e2ffe87a10fae691094b0bfcc1e
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:16:03 +0200

    Update snippet_0002.php

--- chapters/chapter_0009/snippet_0002.php
+++ chapters/chapter_0009/snippet_0002.php
@@ -1,1 +1,16 @@
 <?php
+class Circle implements Shape 
+{
+    private $radius;
+
+    public function __construct($radius)
+    {
+        $this -> radius = $radius;
+    }
+
+    // The method calcArea calculates the area of circles
+    public function calcArea()
+    {
+        return $this -> radius * $this -> radius * pi();
+    }
+}
