73198dea2bbc6c37955f51d390bb49df769288f7
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:19:37 +0200

    Update snippet_0008.php

--- chapters/chapter_0009/snippet_0008.php
+++ chapters/chapter_0009/snippet_0008.php
@@ -1,1 +1,21 @@
 <?php
+abstract class User 
+{
+    protected $scores           = 0;
+    protected $numberOfArticles = 0;
+
+    public function setNumberOfArticles($int)
+    {
+        // Cast to integer type
+        $numberOfArticles = (int)$int;
+        $this -> numberOfArticles = $numberOfArticles;
+    }
+
+    public function getNumberOfArticles()
+    {
+        return $this -> numberOfArticles;
+    }
+    
+    // The abstract method.
+    abstract public function calcScores();
+}
