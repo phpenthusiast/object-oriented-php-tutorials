b0bc4cdb2a11b7b8b16cac005d22f238d5a2fa37
PHPenthusiast <support@phpenthusiast.com>
2021-12-20 21:32:19 +0200

    Update snippet_0006.php

--- chapters/chapter_0010/snippet_0006.php
+++ chapters/chapter_0010/snippet_0006.php
@@ -1,1 +1,32 @@
 <?php
+class car
+{
+  protected $model;
+  protected $hasSunRoof;
+  protected $numberOfDoors;
+  protected $price;
+    
+  // string type hinting
+  public function setModel(string $model) 
+  {
+    $this->model = $model;
+  }
+
+  // boolean type hinting
+  public function setHasSunRoof(bool $value) 
+  {
+    $this->hasSunRoof = $value;
+  }
+
+  // integer type hinting
+  public function setNumberOfDoors(int $value) 
+  {
+    $this->numberOfDoors = $value;
+  }
+    
+  // float type hinting
+  public function setPrice(float $value)
+  {
+    $this->price = $value;
+  }
+}
