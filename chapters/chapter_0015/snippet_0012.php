24e776f3e47511399494339bf64cb2a79858e090
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:17:14 +0200

    Update snippet_0012.php

--- chapters/chapter_0015/snippet_0012.php
+++ chapters/chapter_0015/snippet_0012.php
@@ -1,1 +1,17 @@
 <?php
+class Car 
+{
+    protected $driver;
+
+    // The setDriver method sets
+    //   the value for the driver property
+    public function setDriver(Driver $driver)
+    {
+        $this -> driver = $driver;
+    }
+    
+    public function getDriver()
+    {
+        return $this -> driver;
+    }
+}
