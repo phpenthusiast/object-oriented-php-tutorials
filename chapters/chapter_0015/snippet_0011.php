629bf6a3cfb4ff933c30600b47a76dd4c20d5b29
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:16:41 +0200

    Update snippet_0011.php

--- chapters/chapter_0015/snippet_0011.php
+++ chapters/chapter_0015/snippet_0011.php
@@ -1,1 +1,13 @@
 <?php
+interface Driver 
+{
+    public function sayYourName($name);
+}
+
+class HumanDriver implements Driver 
+{
+    public function sayYourName($name)
+    {
+        return $name;
+    }
+}
