1c06160096e0291860cfc8025b1cafb28a686778
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:15:37 +0200

    Update snippet_0009.php

--- chapters/chapter_0015/snippet_0009.php
+++ chapters/chapter_0015/snippet_0009.php
@@ -1,1 +1,18 @@
 <?php
+// The Car class gets the driver object injected
+//   to its constructor
+class Car 
+{
+    protected $driver;
+    
+    // The constructor sets the value of the $driver
+    public function __construct(Driver $driver)
+    {
+        $this -> driver = $driver;
+    }
+
+    public function getDriver()
+    {
+        return $this -> driver;
+    }
+}
