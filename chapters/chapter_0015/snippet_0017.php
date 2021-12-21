ce898e613ec84060332be9e46d20c00bd0198a6e
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:19:19 +0200

    Update snippet_0017.php

--- chapters/chapter_0015/snippet_0017.php
+++ chapters/chapter_0015/snippet_0017.php
@@ -1,1 +1,24 @@
 <?php
+class Article 
+{
+    protected $title;
+    protected $author;
+    
+    public function __construct($title, $author)
+    {
+        $this -> title = $title;
+        
+        // The object is injected to the class.
+        $this -> author = $author;
+    }
+
+    public function getTitle()
+    {
+        return $this -> title;
+    }
+
+    public function getAuthor()
+    {
+        return $this -> author;
+    }
+}
