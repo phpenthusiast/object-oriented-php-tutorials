ef6e56ba35afab2ce5679ea9e272241874bd0553
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:22:27 +0200

    Update snippet_0024.php

--- chapters/chapter_0015/snippet_0024.php
+++ chapters/chapter_0015/snippet_0024.php
@@ -1,1 +1,9 @@
 <?php
+$author1 = new Author();
+$author1 -> setName("Joe");
+
+$title = "To PHP and Beyond";
+$article1 = new Article($title,$author1);
+
+echo $article1 -> getTitle() . ' by ' . 
+    $article1 -> getAuthor() -> getName();
