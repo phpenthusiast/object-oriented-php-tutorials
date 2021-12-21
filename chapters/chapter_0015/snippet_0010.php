d195bf3c885d4ef96b2e163f611865280e2d7b51
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:16:00 +0200

    Update snippet_0010.php

--- chapters/chapter_0015/snippet_0010.php
+++ chapters/chapter_0015/snippet_0010.php
@@ -1,1 +1,8 @@
 <?php
+$humanDriver = new HumanDriver();
+$car = new Car($humanDriver);
+echo $car -> getDriver() -> sayYourName("Bob");
+
+$robotDriver = new RobotDriver();
+$car = new Car($robotDriver);
+echo $car -> getDriver() -> sayYourName("R2-D2");
