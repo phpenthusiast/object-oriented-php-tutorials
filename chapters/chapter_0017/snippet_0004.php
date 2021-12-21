344623fc2babe74329de17c5e59e833bca735935
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 21:51:24 +0200

    Update snippet_0004.php

--- chapters/chapter_0017/snippet_0004.php
+++ chapters/chapter_0017/snippet_0004.php
@@ -1,1 +1,7 @@
 <?php
+$query->bindParam(':name',$name);
+
+$query->bindParam(':name',$name,PDO::PARAM_STR);
+$query->bindParam(':phone',$phone,PDO::PARAM_INT);
+$query->bindParam(':city',$city,PDO::PARAM_STR);
+$query->bindParam(':date',$date,PDO::PARAM_STR);
