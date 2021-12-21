98b3fbd6fb584e0007345f25a39193d92f91738c
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 22:07:59 +0200

    Update snippet_0035.php

--- chapters/chapter_0017/snippet_0035.php
+++ chapters/chapter_0017/snippet_0035.php
@@ -1,1 +1,12 @@
 <?php
+class User 
+{
+    private $tableName = 'users';
+    private $dbCon;
+
+    // First, save the connection in a private property.
+    function __construct($dbCon)
+    {
+        $this->dbCon = $dbCon;
+    }
+}
