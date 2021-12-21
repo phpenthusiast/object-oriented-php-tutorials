3148e55b161a49f652632ae31b16636585fed181
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 21:58:12 +0200

    Update snippet_0017.php

--- chapters/chapter_0017/snippet_0017.php
+++ chapters/chapter_0017/snippet_0017.php
@@ -1,1 +1,8 @@
 <?php
+if($query -> rowCount() > 0){
+    foreach($results as $result){
+        echo $result -> name . ", ";
+        echo $result -> city . ", ";
+        echo $result -> date_added;
+    }
+}
