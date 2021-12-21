35d20957e63fd2c7f5d9614e13d4795a92cf167a
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 08:55:46 +0200

    Update snippet_0011.php

--- chapters/chapter_0014/snippet_0011.php
+++ chapters/chapter_0014/snippet_0011.php
@@ -1,1 +1,15 @@
 <?php
+// 1. Require both classes
+require "src/Car/CarIntro.php";
+require "src/Car/CarPrice.php";
+
+// 2. Import the namespaces and give them a friendly name
+use Acme\Car\CarIntro as Intro;
+use Acme\Car\CarPrice as Price;
+
+// 3. Use the methods in the context of their namespace
+$carIntro = new Intro();
+echo $carIntro -> sayHello();
+
+$carPrice = new Price();
+echo $carPrice -> getQuote();
