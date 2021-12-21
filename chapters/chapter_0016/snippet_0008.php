031354d4fbb2036f51baad3521683ed05bb3f5b5
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:38:40 +0200

    Update snippet_0008.php

--- chapters/chapter_0016/snippet_0008.php
+++ chapters/chapter_0016/snippet_0008.php
@@ -1,1 +1,17 @@
 <?php
+foreach($dataFromCars as $data => $value) {
+    try {
+        $fuelEconomy = new FuelEconomy();
+        echo $fuelEconomy -> calculate($value[0],$value[1]) . "<br />";
+    }
+    catch (Exception $e) {
+        // Echo the error message to the user
+        echo "Message: " . $e -> getMessage() . "<br />";
+        echo "<hr />";
+
+        // Write the error into a log file
+        error_log($e -> getMessage());
+        error_log($e -> getFile());
+        error_log($e -> getLine());
+    }
+}
