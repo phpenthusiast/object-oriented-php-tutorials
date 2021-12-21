1aa25ee611fe823478f74e12d1bc1442a10d2778
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:21:27 +0200

    Update snippet_0022.php

--- chapters/chapter_0015/snippet_0022.php
+++ chapters/chapter_0015/snippet_0022.php
@@ -1,1 +1,21 @@
 <?php
+interface Authors 
+{
+    public function setName($name);
+    public function getName();
+}
+
+class Author implements Authors 
+{
+    protected $name;
+
+    public function setName($name)
+    {
+        $this -> name = $name;
+    }
+
+    public function getName()
+    {
+        return $this -> name;
+    }
+}
