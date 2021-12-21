d4721ed3ab0b70d5066eb75e8a8208145ed7ef32
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:37:21 +0200

    Update snippet_0006.php

--- chapters/chapter_0016/snippet_0006.php
+++ chapters/chapter_0016/snippet_0006.php
@@ -1,1 +1,16 @@
 <?php
+foreach ($dataFromCars as $data => $value) {
+    try {
+        $fuelEconomy = new FuelEconomy();
+        echo $fuelEconomy -> calculate($value[0],$value[1]) . "<br />";
+    } catch (Exception $e) {
+        echo "Message: " . $e -> getMessage() . "<br />";
+
+        // Output the path to the file
+        echo "File: " . $e -> getFile() . "<br />";
+        
+         // Output the line in the code
+         echo "Line: " . $e -> getLine() . "<br />";
+         echo "<hr />";
+    }
+}
