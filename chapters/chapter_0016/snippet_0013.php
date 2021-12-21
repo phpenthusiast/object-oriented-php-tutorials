2e083745b42f42e30165a5710bc4bb0498c07fc8
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:42:06 +0200

    Update snippet_0013.php

--- chapters/chapter_0016/snippet_0013.php
+++ chapters/chapter_0016/snippet_0013.php
@@ -1,1 +1,22 @@
 <?php
+$dataForUsers = array(
+    array("Ben",4),
+    array("Eva",28),
+    array("li",29),
+    array("Catie","not yet born"),
+    array("Sue",1.5)
+);
+
+foreach($dataForUsers as $data => $value) {
+    try {
+        $user = new User();
+
+        $user -> setName($value[0]);
+        $user -> setAge($value[1]);
+
+        echo $user -> getName() . " is " . $user -> getAge() . " years old. <br />";
+    } catch (Exception $e) {
+        echo "Error: " . $e -> getMessage() . " in the file: " . $e -> getFile()
+                . " on line: " . $e -> getLine() . "<hr />";
+    }
+}	
