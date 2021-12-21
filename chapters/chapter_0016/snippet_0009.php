5bb867538aa335d0791d3f2b6c33da2af2e64cc2
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:39:17 +0200

    Update snippet_0009.php

--- chapters/chapter_0016/snippet_0009.php
+++ chapters/chapter_0016/snippet_0009.php
@@ -1,1 +1,30 @@
 <?php
+class User 
+{
+    private $name;
+    private $age;
+
+    public function setName($name)
+    {
+        // Trim the white spaces
+        $name = trim($name);
+        $this -> name = $name;
+    }
+
+    public function setAge($age)
+    {
+        // Cast into an integer type
+        $age = (int)$age;
+        $this -> age = $age;
+    }
+
+    public function getName()
+    {
+        return $this -> name;
+    }
+    
+    public function getAge()
+    {
+        return $this -> age;
+    }
+}
