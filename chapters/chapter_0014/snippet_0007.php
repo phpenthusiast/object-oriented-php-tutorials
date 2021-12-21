643286a56ce8c6c375c5da51875179de28a323a8
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 08:53:46 +0200

    Update snippet_0007.php

--- chapters/chapter_0014/snippet_0007.php
+++ chapters/chapter_0014/snippet_0007.php
@@ -1,1 +1,10 @@
 <?php
+// 1. Require the file
+require "src/Car/CarIntro.php";
+
+// 2. Import the namespace and give it a friendly name
+use Acme\Car\CarIntro as Intro;
+
+// 3. Call the class from the namespace with the friendly name
+$carIntro = new Intro();
+echo $carIntro -> sayHello();
