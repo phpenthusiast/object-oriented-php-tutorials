eb28590f2753c4acace07cfbf12224b63e8d30a2
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 21:22:55 +0200

    Update snippet_0003.php

--- chapters/chapter_0020/snippet_0003.php
+++ chapters/chapter_0020/snippet_0003.php
@@ -1,1 +1,18 @@
 <?php
+class Model
+{
+    // Store the data
+    private $carPrice;
+
+    // Set the data
+    public function setPrice($price)
+    {
+        $this -> carPrice = $price;
+    }
+
+    // Get the data
+    public function getPrice()
+    {
+        return $this -> carPrice;
+    }
+}
