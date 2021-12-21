a7194a1ec9f8a2271bc5ca35b7f7121e5960529c
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:11:28 +0200

    Update snippet_0001.php

--- chapters/chapter_0015/snippet_0001.php
+++ chapters/chapter_0015/snippet_0001.php
@@ -1,1 +1,30 @@
 <?php
+class HumanDriver 
+{
+    // Method to return the driver name
+    public function sayYourName($name)
+    {
+        return $name;
+    }
+}
+
+
+// Car is dependent on HumanDriver
+class Car 
+{
+    protected $driver;
+
+    // We create the driver object in the constructor,
+    //   and use this object to populate the $driver property
+    public function __construct()
+    {
+        $this->driver = new HumanDriver();
+    }
+    
+    // A getter method that returns the driver object
+    //   from within the car object
+    public function getDriver()
+    {
+        return $this -> driver;
+    }
+}
