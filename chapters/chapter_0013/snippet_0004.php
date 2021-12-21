d9961ef82071ea3bf4191e062eee36f55dab76c8
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 08:04:35 +0200

    Update snippet_0004.php

--- chapters/chapter_0013/snippet_0004.php
+++ chapters/chapter_0013/snippet_0004.php
@@ -1,1 +1,11 @@
 <?php
+$bmw1 = new Bmw();
+
+// Add 3000$
+echo $bmw1 -> changePriceByDollars(45000, +3000);
+
+
+$mercedes1 = new Mercedes();
+
+// Subtract 2100$
+echo $mercedes1 -> changePriceByDollars(42000, -2100);
