6d418ab3e6b8cce15db7c0c57e6f740527bdd0d3
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 08:06:54 +0200

    Update snippet_0006.php

--- chapters/chapter_0013/snippet_0006.php
+++ chapters/chapter_0013/snippet_0006.php
@@ -1,1 +1,34 @@
 <?php
+trait Price 
+{
+    // The method needs the $price property.
+    public function changePriceByDollars($change)
+    {
+        return $this -> price += $change;
+    }
+}
+
+
+class Mercedes 
+{
+    use Price;
+
+    // The $price is private
+    private $price;
+    
+    public function __construct($price)
+    {
+        $this -> price = $price;
+    }
+
+    public function getPrice()
+    {
+        return $this -> price;
+    }
+}
+
+
+$mercedes1 = new Mercedes(42000);
+echo $mercedes1 -> getPrice();
+$mercedes1 -> changePriceByDollars(-2100);
+echo $mercedes1 -> getPrice();
