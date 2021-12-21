3a4890c8c8f39a8ec8653b8a75f704a0c7c7ff78
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 21:58:40 +0200

    Update snippet_0018.php

--- chapters/chapter_0017/snippet_0018.php
+++ chapters/chapter_0017/snippet_0018.php
@@ -1,1 +1,20 @@
 <?php
+$sql = "SELECT * FROM users WHERE city = :city";
+
+$query = $dbh -> prepare($sql);
+
+$query -> bindParam(':city', $city, PDO::PARAM_STR);
+
+$city = "New York";
+
+$query -> execute();
+
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+
+if($query -> rowCount() > 0){
+    foreach($results as $result){
+        echo $result -> name . ", ";
+        echo $result -> city . ", ";
+        echo $result -> date_added;
+    }
+}
