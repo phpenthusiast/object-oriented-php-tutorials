98da3a4cef562208d3ec14b5fa1a0a903a901be8
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:15:06 +0200

    Update snippet_0008.php

--- chapters/chapter_0015/snippet_0008.php
+++ chapters/chapter_0015/snippet_0008.php
@@ -1,1 +1,24 @@
 <?php
+// The Driver interface
+interface Driver 
+{
+    public function sayYourName($name);
+}
+
+// The HumanDriver implements the interface
+class HumanDriver implements Driver 
+{
+    public function sayYourName($name)
+    {
+        return $name;
+    }
+}
+
+// The RobotDriver implements the interface
+class RobotDriver implements Driver 
+{
+    public function sayYourName($name)
+    {
+        return $name;
+    }
+}
