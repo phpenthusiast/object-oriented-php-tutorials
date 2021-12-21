909944e41b3a36854ea3dc03b3da288064aafa30
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:34:44 +0200

    Update snippet_0004.php

--- chapters/chapter_0016/snippet_0004.php
+++ chapters/chapter_0016/snippet_0004.php
@@ -1,1 +1,15 @@
 <?php
+class FuelEconomy 
+{
+    // Calculate the fuel efficiency
+    public function calculate($distance, $gas)
+    {
+        if($gas <= 0 ) {
+            // Throw custom error message, instead of an error
+            throw new Exception("The gas consumption cannot be less than 
+               or equal to zero. You better drink coffee or take a break.");
+        }
+
+        return $distance/$gas;
+    }
+}
