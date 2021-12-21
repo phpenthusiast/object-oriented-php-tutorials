321d3d9e21d653f46f1ef80442d1f5cd336913fd
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 22:06:47 +0200

    Update snippet_0032.php

--- chapters/chapter_0017/snippet_0032.php
+++ chapters/chapter_0017/snippet_0032.php
@@ -1,1 +1,17 @@
 <?php
+$sql   = "DELETE FROM `users` WHERE `id`=:id";
+
+$query = $dbh -> prepare($sql);
+
+$query -> bindParam(':id', $id, PDO::PARAM_INT);
+
+$id = 1;
+
+$query -> execute();
+
+if ($query -> rowCount() > 0) {
+    $count = $query -> rowCount();
+    echo $count . " rows were affected.";
+} else {
+    echo "No affected rows.";
+}
