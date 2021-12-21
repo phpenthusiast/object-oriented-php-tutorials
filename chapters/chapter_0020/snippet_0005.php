3ee02af9529006774f517d8faba6f781ba0e7f69
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 21:23:53 +0200

    Update snippet_0005.php

--- chapters/chapter_0020/snippet_0005.php
+++ chapters/chapter_0020/snippet_0005.php
@@ -1,1 +1,17 @@
 <?php
+class View 
+{
+    private $controller;
+
+    // Set the controller so we can use it
+    public function __construct(Controller $controller)
+    {
+        $this->controller = $controller;
+    }
+
+    // Output the data after processing it by the controller
+    public function output()
+    {
+        return $this->controller->expensiveOrNot();
+    }
+}
