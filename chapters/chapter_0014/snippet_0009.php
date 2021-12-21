e651a56930a4c164ee1914968d2e25047ccca9e4
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 08:54:52 +0200

    Update snippet_0009.php

--- chapters/chapter_0014/snippet_0009.php
+++ chapters/chapter_0014/snippet_0009.php
@@ -1,1 +1,13 @@
 <?php
+require "src/Car/CarIntro.php";
+require "src/Car/CarPrice.php";
+
+use Acme\Car\CarIntro as Intro;
+
+$carIntro = new Intro();
+echo $carIntro -> sayHello();
+
+// We use the backslash in front of the class name in order to call a class
+//   from the global namespace
+$carPrice = new \CarPrice();
+echo $carPrice -> getQuote();
