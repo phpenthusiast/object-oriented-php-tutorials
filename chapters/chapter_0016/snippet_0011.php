fafebef27b8d07e2af37c6114835ba80480311b0
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:40:30 +0200

    Update snippet_0011.php

--- chapters/chapter_0016/snippet_0011.php
+++ chapters/chapter_0016/snippet_0011.php
@@ -1,1 +1,33 @@
 <?php
+class User 
+{
+    private $name;
+    private $age;
+
+    public function setName($name)
+    {
+        $name = trim($name);
+
+        if(strlen($name) < 3)
+        {
+            throw new Exception("The name should be at least 3 characters long.");
+        }
+
+        $this -> name = $name;
+    }
+
+    public function setAge($age)
+    {
+        $this -> age = $age;
+    }
+
+    public function getName()
+    {
+        return $this -> name;
+    }
+    
+    public function getAge()
+    {
+        return $this -> age;
+    }
+}
