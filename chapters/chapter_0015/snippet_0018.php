01698fac0e1cc837808bba017ad3fe43366e0400
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:19:46 +0200

    Update snippet_0018.php

--- chapters/chapter_0015/snippet_0018.php
+++ chapters/chapter_0015/snippet_0018.php
@@ -1,1 +1,15 @@
 <?php
+class Author 
+{
+    protected $name;
+
+    public function setName($name)
+    {
+        $this -> name = $name;
+    }
+
+    public function getName()
+    {
+        return $this -> name;
+    }
+}
