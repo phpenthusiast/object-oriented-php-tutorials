3962b756480f84220b822d533470c10038b050ad
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:14:08 +0200

    Update snippet_0006.php

--- chapters/chapter_0015/snippet_0006.php
+++ chapters/chapter_0015/snippet_0006.php
@@ -1,1 +1,18 @@
 <?php
+class Car 
+{
+    protected $driver;
+
+    // The constructor sets the value of the $driver
+    public function __construct($driver)
+    {
+        $this -> driver = $driver;
+    }
+    
+    // A getter method that returns the driver object
+    //   from within the car object
+    public function getDriver()
+    {
+        return $this -> driver;
+    }
+}
