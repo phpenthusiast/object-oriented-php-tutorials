3ad803c36c895e49670130dfe2b65dbcf02955b4
PHPenthusiast <support@phpenthusiast.com>
2021-12-21 09:35:58 +0200

    Update snippet_0005.php

--- chapters/chapter_0016/snippet_0005.php
+++ chapters/chapter_0016/snippet_0005.php
@@ -1,1 +1,38 @@
 <?php
+class FuelEconomy 
+{
+    // Calculate the fuel efficiency
+    public function calculate($distance, $gas)
+    {
+        if($gas <= 0 ) {
+            // Throw custom error message, instead of an error
+            throw new Exception("The gas consumption cannot be less than 
+               or equal to zero. You better drink coffee or take a break.");
+        }
+
+        return $distance/$gas;
+    }
+}
+
+// The data to feed the class with
+$dataFromCars = array(
+    array(50,10),
+    array(50,0),
+    array(50,-3),
+    array(30,5)
+);
+
+// Create the object from the class and feed it
+//   with the array values
+foreach($dataFromCars as $data => $value) {
+        // Try block handles the normal data
+    try {
+        $fuelEconomy = new FuelEconomy();
+        echo $fuelEconomy -> calculate($value[0],$value[1]) . "<br />";
+    }
+    // Catch block handles the exceptions
+    catch (Exception $e) {
+        // Echo the custom error message
+        echo "Message: " . $e -> getMessage() . "<br />"; 
+    }
+}
